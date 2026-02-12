34b187cd1a71536812eeee0a4120ab05528ff14f
samheckle <samheckle@yahoo.com>
2026-02-12 13:31:30 -0500

    added class 8 starter

---
+++ class-notes/class8/class8-starter/main.js
@@ -0,0 +1,8 @@
+// Q: what is the first function we always run in our javascript files? add it below this comment.
+
+// Q: inside the function, get the element with the id of "rotate" and store it in a variable.
+
+// Q: what function do we use to repeat something over time?
+// A:
+// Q: what parameters does it accept?
+// A:
