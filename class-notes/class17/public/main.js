b704a8211bd86d3ab969b9b071ac9d20a359a99d
samheckle <samheckle@yahoo.com>
2026-03-27 17:08:52 -0400

    added class 17 demo

---
+++ class-notes/class17/public/main.js
@@ -0,0 +1,24 @@
+window.onload = async () => {
+	let contentDiv = document.getElementById('content');
+
+	let url = '/api/notes';
+
+	let response = await fetch(url);
+	let notes = await response.json();
+
+	console.log(notes);
+
+	for (let n of notes) {
+		console.log(n);
+
+		let newDiv = document.createElement('div');
+		newDiv.classList.add('note');
+		newDiv.id = n._id;
+
+		let text = document.createElement('p');
+		text.textContent = n.note;
+
+		newDiv.appendChild(text);
+		contentDiv.appendChild(newDiv);
+	}
+};
---
+++ class-notes/class17/server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+
+// new library!
+const nedb = require('@seald-io/nedb');
+
+const app = express();
+
+// new! initialize database
+const database = new nedb({ filename: 'mydatabase.txt', autoload: true });
+
+// old! middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (request, response) => {
+	// .insert() has 2 parameters
+	// 1: object to be added
+	let dataToBeAdded = {
+		text: 'hi',
+	};
+	// 2: callback function, action to happen once data has been added (optional)
+	database.insert(dataToBeAdded);
+
+	response.send('<h1>server is working!</h1>');
+});
+
+app.post('/makePost', (request, response) => {
+	// retrieve client data from request
+	// request.body is undefined without using app.use(express.urlencoded({ extended: true }));
+	console.log(request.body);
+
+	let dataToBeAdded = {
+		note: request.body.note,
+	};
+
+	// using the database.insert() replaces the global array we used in the past
+	database.insert(dataToBeAdded);
+
+	response.redirect('/post.html');
+});
+
+app.get('/api/entire-database', (request, response) => {
+	// database.find() has 2 parameters
+	// 1. object we are looking for
+	// if we pass in empty object, we will get the entire database
+	let query = {};
+	// 2. callback function
+	database.find(query, (err, foundData) => {
+		if (err) {
+			response.send('error in retrieving data');
+		} else {
+			response.json(foundData);
+		}
+	});
+});
+
+app.get('/api/notes', (request, response) => {
+	// if we want to find all instances of a matching value
+	let query = {
+		note: { $exists: true },
+	};
+
+	database.find(query, (err, foundData) => {
+		if (err) {
+			response.send('error in retrieving data');
+		} else {
+			response.json(foundData);
+		}
+	});
+});
+
+app.listen(4005, () => {
+	console.log('app is running on http://localhost:4005');
+});
