46bdca0304b49fb3e70e27529467b23f8ee42242
samheckle <samheckle@yahoo.com>
2026-03-31 17:14:35 -0400

    added class 18

--- class-notes/class18/bot.js
+++ class-notes/class18/bot.js
@@ -1,30 +1,34 @@
+// imports the dotenv library
+// and allows us to access variables inside .env file
+// by using process.env.VARIABLE_NAME
 require('dotenv').config();
+// importing the masto api that we will use
 const m = require('masto');
 
+// setup the ability to use the masto library
+// this is very similar to making app
+// const app = express()
 const masto = m.createRestAPIClient({
-	url: 'https://networked-media.itp.io/',
+	url: 'https://networked-media.itp.io',
 	accessToken: process.env.TOKEN,
 });
 
-async function makeStatus(text) {
-	const status = await masto.v1.statuses.create({
-		// the thing that will be posted
-		status: text,
-		// change the visibility for testing purposes
+// function makeStatus(){}
+const makeStatus = async () => {
+	// customize the text output to be random when we run the function
+	let emojis = ['🫡', '💖', '🎉', '🤠'];
+	// this grabs a random index between 0 - emoji.length
+	// this number needs to be a whole number
+	let randomSelection = Math.floor(Math.random() * emojis.length);
+
+	const s = await masto.v1.statuses.create({
+		status: emojis[randomSelection],
 		visibility: 'private',
 	});
+	console.log(s.url);
+};
 
-	console.log(status.url);
-}
-
-// makeStatus("hi")
-
-setInterval(() => {
-	let emoji = ['😭', '🤩', '💖'];
-
-	let rand = Math.floor(Math.random() * emoji.length);
-
-	let post = emoji[rand];
-
-	makeStatus(post);
-}, 5000);
+// will post one status one time
+// makeStatus();
+// will post a status one time every 10 seconds
+setInterval(makeStatus, 10000);
